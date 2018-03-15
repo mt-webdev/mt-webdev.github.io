70a53919def7ce293c558404e27680ce1fdd9830
Marco <mtraspel@hotmail.de>
2018-03-15 14:08:05 +0100

    fixed print view

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,16 +1,18 @@
-const appCache = 'mtwd-cv-0.1.05';
+const appCache = 'mtwd-cv-0.1.06';
 
 const CACHED_FILES = [
     '/',
     'templates/home.html',
     'templates/notFound.html',
     'main.bundle.js',
-    'vendor.bundle.js',
-    'polyfills.bundle.js',
     'main.css',
+    'vendor.bundle.js',
+    'vendor.css',
+    'polyfills.bundle.js',
     'images/MTWD_384x384.svg',
     'images/picture.jpg',
-    'vendor.css'
+    'styles.bundle.js',
+    'styles.css'
 ];
 
 
